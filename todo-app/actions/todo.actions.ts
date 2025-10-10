67b0d53c4adbf3681a31112a5de5db6b91700823
hmmam-ziad <hmmamxxc@gmail.com>
2025-10-10 17:48:44 +0300

    init the database and connection

---
+++ todo-app/actions/todo.actions.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTodosActions = async () => {
+
+}
+
+
+export const createTodoActions = async () => {
+    
+}
+
+export const updateTodoActions = async () => {
+    
+}
+
+export const deleteTodoActions = async () => {
+    
+}
---
+++ todo-app/prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+    await prisma.post.createMany({
+        data: Array.from({ length: 25 }, () => {
+            return {
+                title: faker.lorem.sentence(),
+                body: faker.lorem.paragraph(),
+                completed: faker.datatype.boolean(),
+            };
+        })
+    });
+
+    // await prisma.user.createMany({
+    //         data: Array.from({ length: 25 }, () => {
+    //             return {
+    //                 email: faker.internet.email(),
+    //                 name: faker.internet.username(),
+    //                 prfileImage: "https://avatars.githubusercontent.com/u/44652353?v=4",
+    //                 address: {
+    //                     street: faker.location.street(),
+    //                     city: faker.location.city(),
+    //                     state: faker.location.state(),
+    //                     zipCode: faker.location.zipCode(),
+    //                 },
+    //             };
+    //         })
+    //     });
+}
+
+main()
+    .catch(async e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
