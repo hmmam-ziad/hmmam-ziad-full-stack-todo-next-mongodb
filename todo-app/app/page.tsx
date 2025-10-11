d8ac7d9eea1c52fa88e1406564d3075530227fce
hmmam-ziad <hmmamxxc@gmail.com>
2025-10-11 15:10:45 +0300

    todos table

--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -1,20 +1,19 @@
+import { getTodosActions } from "@/actions/todo.actions";
 import AddTodoForm from "@/components/AddTodoForm";
+import TodoTable from "@/components/TodoTable";
 
-export default function Home() {
-
-  
-
-  
-
+export default async function Home() {
+  const todos = await getTodosActions(); 
   return (
     <main className="container">
-    <AddTodoForm />      
+    <AddTodoForm />
+    <TodoTable />
 
-    {/* <ul>
+    <ul>
       {
         todos.map(todo => <li key={todo.id} >{ todo.title}</li>)
       }
-    </ul> */}
+    </ul>
     </main>
   );
 }
